6fef4197792126a29dc6d51e90ce48fdc0230ae2
D Jones <djones@Ds-MacBook-Air.mynetworksettings.com>
2023-11-08 21:11:12 -0500

    movie comp complete

--- src/component/MovieDetail.jsx
+++ src/component/MovieDetail.jsx
@@ -15,7 +15,7 @@ useEffect(() => {
         setMovie(responseJSON)
     })
     .catch(error => console.log(error))
-}, {is, API})
+}, {id, API})
 
 const handleDelete = () => {
     deleteMovie()
@@ -29,8 +29,76 @@ const deleteMovie = () => {
 }
 
   return (
-    <div>
-      
+    <div className="movie-detail-container">
+      <div className="sidebar">
+        <button onClick={handleDelete}>Delete</button>
+        <Link to={`/edit/${movie.id}`}>Edit</Link>
+        <button onClick={() => navigate(-1)}>Back</button>
+        <Link to={`/tasks/${movie.id}`}>New Task</Link>
+      </div>
+      <div className='movie-details'>
+    <table>
+      <tbody>
+        <tr>
+          <th>Studio</th>
+          <td>{movie.studio}</td>
+        </tr>
+        <tr>
+          <th>Overview</th>
+          <td>{movie.overview}</td>
+        </tr>
+        <tr>
+          <th>Balance</th>
+          <td>{movie.balance}</td>
+        </tr>
+        <tr>
+          <th>Budget</th>
+          <td>{movie.budget}</td>
+        </tr>
+        <tr>
+          <th>Director</th>
+          <td>{movie.director}</td>
+        </tr>
+        <tr>
+          <th>Staring</th>
+          <td>{movie.staring}</td>
+        </tr>
+        <tr>
+          <th>Genre</th>
+          <td>{movie.genre}</td>
+        </tr>
+        <tr>
+Schedule          <td>{movie.schedule}</td>
+        </tr>
+        <tr>
+          <th>Release Year</th>
+          <td>{movie.releaseYear}</td>
+        </tr>
+        <tr>
+          <th>Tasks</th>
+          <td></td>
+        </tr>
+        <tr>
+          <th>description</th>
+          <td>{movie.description}</td>
+        </tr>
+        <tr>
+          <th>Department</th>
+          <td>{movie.department}</td>
+        </tr>
+        <tr>
+          <th>Cost</th>
+          <td>{movie.cost}</td>
+        </tr>
+        <tr>
+          <th>Edit</th>
+          <td>
+            <Link to={`/movies/${movie.id}`}></Link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
     </div>
   )
 }
