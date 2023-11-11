cf64afe9ba65d076a46657c3de75c87914798e2b
D Jones <djones@Ds-MacBook-Air.mynetworksettings.com>
2023-11-11 00:11:52 -0500

    corrections

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -7,14 +7,11 @@ export default function NavBar() {
     <div className="navbar" >
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark  ">
         
-          <h1 >
             <Link to="/">
               <svg xmlns="http://www.w3.org/2000/svg" width="40" height="35" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
                 <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
               </svg> 
             </Link>
-          </h1>
-          <h1 >
             <Link to="/movies">
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="35" fill="currentColor" className="bi bi-camera-reels" viewBox="0 0 16 16">
                 <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM1 3a2 2 0 1 0 4 0 2 2 0 0 0-4 0z"/>
@@ -22,15 +19,12 @@ export default function NavBar() {
                 <path d="M9 6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM7 3a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
               </svg>
             </Link>
-          </h1>
-          <h1 >
             <Link to="/movies/new">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="35" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
               <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
               <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
               </svg>
             </Link>
-          </h1>
         <div className="container">
           " "
           <a className="navbar-brand" href="#">
