b8b5d8a6d7fe7072253fdbccaab651445e9fd843
D Jones <djones@Ds-MacBook-Air.mynetworksettings.com>
2023-11-11 02:09:52 -0500

    edit form in progress

--- src/Pages/Edit.jsx
+++ src/Pages/Edit.jsx
@@ -1,10 +1,14 @@
 import MovieEditForm from "../component/MovieEditForm";
+import "./Edit.css"
+
+
 function Edit() {
 
   return (
-    <div className="New Edit">
+    <div className="new-edit">
       <h2>Edit</h2>
       <MovieEditForm />
+      <img src="/edit.png" />
     </div>
   );
 }
