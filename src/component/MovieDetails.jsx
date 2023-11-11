b333f31ffd9019f1492ace37e2529048578e2600
D Jones <djones@Ds-MacBook-Air.mynetworksettings.com>
2023-11-11 15:11:35 -0500

    removed dummy file

--- src/component/MovieDetails.jsx
+++ src/component/MovieDetails.jsx
@@ -1,33 +1,16 @@
-import { Link, useNavigate } from 'react-router-dom';
-import "./MovieDetails.css"
+import { Link, useNavigate } from "react-router-dom";
+import "./MovieDetails.css";
 const API = import.meta.env.VITE_API_URL;
 
-export default function MovieDetails({ movie}) {
-        const navigate = useNavigate();
+export default function MovieDetails({ movie }) {
+  const navigate = useNavigate();
 
-        const handleDelete = () => {
-          const httpOptions = { method: 'DELETE' };
-          fetch(`${API}/movies/${movie.id}`, httpOptions)
-            .then(() => navigate('/movies'))
-            .catch((error) => console.log(error));
-        };
-
-// useEffect(() => {
-//     fetch(`${API}/movies/${id}`)
-//     .then ((response) => response.json())
-//     .then((responseJSON) => {
-//         setMovie(responseJSON)
-//     })
-//     .catch(error => console.log(error))
-// }, {id, API})
-
-
-// const deleteMovie = () => {
-//     const httpOptions = { method: "DELETE" }
-//     fetch(`${API}/movies/${id}`, httpOptions)
-//     .then(() => navigate(`/movies`))
-//     .catch(error => console.log(error))
-// }
+  const handleDelete = () => {
+    const httpOptions = { method: "DELETE" };
+    fetch(`${API}/movies/${movie.id}`, httpOptions)
+      .then(() => navigate("/movies"))
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div className="movie-detail-container">
@@ -37,70 +20,70 @@ export default function MovieDetails({ movie}) {
         <button onClick={() => navigate(-1)}>Back</button>
         <Link to={`/tasks/${movie.id}`}>New Task</Link>
       </div>
-      <div className='movie-details'>
-    <table>
-      <tbody>
-        <tr>
-          <th>Studio</th>
-          <td>{movie.studio}</td>
-        </tr>
-        <tr>
-          <th>Overview</th>
-          <td>{movie.overview}</td>
-        </tr>
-        <tr>
-          <th>Balance</th>
-          <td>{movie.balance}</td>
-        </tr>
-        <tr>
-          <th>Budget</th>
-          <td>{movie.budget}</td>
-        </tr>
-        <tr>
-          <th>Director</th>
-          <td>{movie.director}</td>
-        </tr>
-        <tr>
-          <th>Staring</th>
-          <td>{movie.staring}</td>
-        </tr>
-        <tr>
-          <th>Genre</th>
-          <td>{movie.genre}</td>
-        </tr>
-        <tr>
-          <th>Schedule</th>
-          <td>{movie.schedule}</td>
-        </tr>
-        <tr>
-          <th>Release Year</th>
-          <td>{movie.releaseYear}</td>
-        </tr>
-        <tr>
-          <th>Tasks</th>
-          <td></td>
-        </tr>
-        <tr>
-          <th>description</th>
-          <td>{movie.description}</td>
-        </tr>
-        <tr>
-          <th>Department</th>
-          <td>{movie.department}</td>
-        </tr>
-        <tr>
-          <th>Cost</th>
-          <td>{movie.cost}</td>
-        </tr>
-        <tr>
-          <th>Edit</th>
-          <td>
-            <Link to={`/movies/${movie.id}`}></Link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      <div className="movie-details">
+        <table>
+          <tbody>
+            <tr>
+              <th>Studio</th>
+              <td>{movie.studio}</td>
+            </tr>
+            <tr>
+              <th>Overview</th>
+              <td>{movie.overview}</td>
+            </tr>
+            <tr>
+              <th>Balance</th>
+              <td>{movie.balance}</td>
+            </tr>
+            <tr>
+              <th>Budget</th>
+              <td>{movie.budget}</td>
+            </tr>
+            <tr>
+              <th>Director</th>
+              <td>{movie.director}</td>
+            </tr>
+            <tr>
+              <th>Staring</th>
+              <td>{movie.staring}</td>
+            </tr>
+            <tr>
+              <th>Genre</th>
+              <td>{movie.genre}</td>
+            </tr>
+            <tr>
+              <th>Schedule</th>
+              <td>{movie.schedule}</td>
+            </tr>
+            <tr>
+              <th>Release Year</th>
+              <td>{movie.releaseYear}</td>
+            </tr>
+            <tr>
+              <th>Tasks</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>description</th>
+              <td>{movie.description}</td>
+            </tr>
+            <tr>
+              <th>Department</th>
+              <td>{movie.department}</td>
+            </tr>
+            <tr>
+              <th>Cost</th>
+              <td>{movie.cost}</td>
+            </tr>
+            <tr>
+              <th>Edit</th>
+              <td>
+                <Link to={`/movies/${movie.id}`}></Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
