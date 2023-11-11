19c88c58c122fe9b8ad6037b2718e32983c2a03b
arim83 <jorammercado@pursuit.org>
2023-11-11 15:02:55 -0500

    working on home page, needs formatting

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,9 @@ function Home() {
     <div className="animate__bounceIn">
       <h2>Welcome</h2>
       <h3>Production apprentice</h3>
+      <video width="320" height="240" controls autoplay="true" muted loop playinline>
+        <source src="/public/home_film.mp4" type="video/mp4"/>
+      </video>
     </div>
   );
 }
