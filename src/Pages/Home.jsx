cda97921e691f82fd75f37a9fefbeddacb6cdd1b
arim83 <jorammercado@pursuit.org>
2023-11-11 23:46:33 -0500

    format home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,15 +4,21 @@ function Home() {
   
   return (
     <div className="animate__bounceIn">
-      <h2>Production Apprentice</h2>
-      <h3>Bringing Movie Prouctions to Life</h3>
+      <div className="homeTitle">
+        <div className="topTitle">
+          Production Apprentice
+        </div>
+        <div className="subTitle">
+          Bringing Movie Prouctions to Life
+        </div>
+      </div>
       <div className="grid-container">
         <div>
           <video className="video" width="900" height="500" controls autoPlay={true} muted loop >
             <source src="/public/home_film.mp4" type="video/mp4"/>
           </video>
         </div>
-        <div>
+        <div className="logoDiv">
           <img className="image" src="/public/logo.png" alt="logo" width="500" height="500"/>
         </div>
       </div>
