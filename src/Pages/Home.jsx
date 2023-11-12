2f4d98eeea39f42690ae9d9ef3d116619ae20acb
arim83 <jorammercado@pursuit.org>
2023-11-11 19:35:57 -0500

    working on edit form

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,15 +6,16 @@ function Home() {
     <div className="animate__bounceIn">
       <h2>Production Apprentice</h2>
       <h3>Bringing Movie Prouctions to Life</h3>
-      <div>
-      <video className="video" width="900" height="500" controls autoplay="true" muted loop playinline>
-        <source src="/public/home_film.mp4" type="video/mp4"/>
-      </video>
-      
-      <img className="image" src="/public/logo.png" alt="logo" width="500" height="500"/>
-      
+      <div className="grid-container">
+        <div>
+          <video className="video" width="900" height="500" controls autoPlay={true} muted loop >
+            <source src="/public/home_film.mp4" type="video/mp4"/>
+          </video>
+        </div>
+        <div>
+          <img className="image" src="/public/logo.png" alt="logo" width="500" height="500"/>
+        </div>
       </div>
-      
     </div>
   );
 }
--- src/component/MovieEditForm.jsx
+++ src/component/MovieEditForm.jsx
@@ -1,70 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "./MovieEditForm.css";
 
-const API = import.meta.env.VITE_BASE_URL;
-
+const API = import.meta.env.VITE_API_URL;
 
 export default function MovieEditForm() {
   let { id } = useParams();
   const navigate = useNavigate();
   const [movie, setMovie] = useState({
-        movie_name: '',
-        poster_link: '',
-        director: '',
-        starring: '',
-        overview: '',
-        runtime: 0,
-        release_year: 0,
-        budget: 0,
-        current_balance: 0,
-        schedule: '',
-        in_production: false,
-        studio: '',
-        genre: ''
+    poster_link: "",
+    director: "",
+    starring: "",
+    overview: "",
+    runtime: 0,
+    release_year: 0,
+    budget: 0,
+    current_balance: 0,
+    schedule: "",
+    in_production: false,
   });
 
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
     setMovie({
       ...movie,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === "checkbox" ? checked : value,
     });
   };
 
   const updateMovie = () => {
     fetch(`${API}/movies/${id}`, {
-      method: 'PUT',
+      method: "PUT",
       body: JSON.stringify(movie),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
-    .then((response) => {
-      if (response.ok) {
-        navigate(`/movies/${id}`);
-      } else {
-        throw new Error('Network response was not ok.');
-      }
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+      .then((response) => {
+        if (response.ok) {
+          navigate(`/movies/${id}`);
+        } else {
+          throw new Error("Network response was not ok.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   useEffect(() => {
-    console.log(id)
     fetch(`${API}/movies/${id}`)
       .then((response) => response.json())
       .then((responseJSON) => {
-        console.log(responseJSON)
         setMovie(responseJSON);
-        console.log(responseJSON)
       })
-      .catch((error) => console.error('Error:', error));
+      .catch((error) => console.error("Error:", error));
   }, [id]);
 
   const handleSubmit = (e) => {
@@ -72,141 +66,146 @@ export default function MovieEditForm() {
     updateMovie();
   };
 
+  const handleBack = () => {
+    navigate(`/movies/${id}`)
+  }
+
   return (
-    <Form noValidate onSubmit={handleSubmit}>
-      <Row className="mb-3">
-              <Form.Group as={Col} controlId="movie_name">
-          <Form.Label>Code Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter movie name"
-            name="poster_link"
-            value={movie.movie_name}
+    <div className="form-edit-container">
+      <Form noValidate onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="poster_link">
+            <Form.Label>Poster Link</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter poster URL"
+              name="poster_link"
+              value={movie.poster_link}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="director">
+            <Form.Label>Director</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter director's name"
+              name="director"
+              value={movie.director}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="starring">
+            <Form.Label>Starring</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter main cast"
+              name="starring"
+              value={movie.starring}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="runtime">
+            <Form.Label>Runtime (minutes)</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter runtime in minutes"
+              name="runtime"
+              value={movie.runtime}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="release_year">
+            <Form.Label>Release Year</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter release year"
+              name="release_year"
+              value={movie.release_year}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="budget">
+            <Form.Label>Budget ($)</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter budget"
+              name="budget"
+              value={movie.budget}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="current_balance">
+            <Form.Label>Current Balance ($)</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter current balance"
+              name="current_balance"
+              value={movie.current_balance}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="overview">
+            <Form.Label>Overview</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Enter movie overview"
+              name="overview"
+              value={movie.overview}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="schedule">
+            <Form.Label>Schedule</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter schedule details"
+              name="schedule"
+              value={movie.schedule}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+        </Row>
+        <Form.Group className="mb-3" controlId="in_production">
+          <Form.Check
+            type="checkbox"
+            label="In Production"
+            name="in_production"
+            checked={movie.in_production}
             onChange={handleInputChange}
           />
         </Form.Group>
-              <Form.Group as={Col} controlId="poster_link">
-          <Form.Label>Poster Link</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter poster URL"
-            name="poster_link"
-            value={movie.poster_link}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-      </Row>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="director">
-          <Form.Label>Director</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter director's name"
-            name="director"
-            value={movie.director}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="starring">
-          <Form.Label>Starring</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter main cast"
-            name="starring"
-            value={movie.starring}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-      </Row>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="runtime">
-          <Form.Label>Runtime (minutes)</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Enter runtime in minutes"
-            name="runtime"
-            value={movie.runtime}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="release_year">
-          <Form.Label>Release Year</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Enter release year"
-            name="release_year"
-            value={movie.release_year}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-      </Row>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="budget">
-          <Form.Label>Budget ($)</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Enter budget"
-            name="budget"
-            value={movie.budget}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="current_balance">
-          <Form.Label>Current Balance ($)</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Enter current balance"
-            name="current_balance"
-            value={movie.current_balance}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-      </Row>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="overview">
-          <Form.Label>Overview</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={3}
-            placeholder="Enter movie overview"
-            name="overview"
-            value={movie.overview}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-      </Row>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="schedule">
-          <Form.Label>Schedule</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter schedule details"
-            name="schedule"
-            value={movie.schedule}
-            onChange={handleInputChange}
-          />
-        </Form.Group></Row>
-      <Form.Group className="mb-3" controlId="in_production">
-        <Form.Check 
-          type="checkbox"
-          label="In Production"
-          name="in_production"
-          checked={movie.in_production}
-          onChange={handleInputChange}
-        />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Update Movie
-      </Button>
-    </Form>
-  )
+        <div className="form-edit-container-button">
+          <div>
+          <Button className="update" variant="primary" type="submit">
+            Update Movie
+          </Button>
+          </div>
+          <div>
+          <Button variant="secondary" onClick={handleBack} type="submit">
+            Back
+          </Button>
+          </div>
+        </div>
+      </Form>
+    </div>
+  );
 }
